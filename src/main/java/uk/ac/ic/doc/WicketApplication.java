f96d84244b7f302c17ac3ca708bab40e59fba4ee
Adam Ishaq <adam.ishaq09@imperial.ac.uk>
2012-01-20 13:51:40 +0000

    First correct commit

---
+++ src/main/java/uk/ac/ic/doc/WicketApplication.java
@@ -0,0 +1,31 @@
+package uk.ac.ic.doc;
+
+import org.apache.wicket.protocol.http.WebApplication;
+
+/**
+ * Application object for your web application. If you want to run this application without deploying, run the Start class.
+ * 
+ * @see uk.ac.ic.doc.Start#main(String[])
+ */
+public class WicketApplication extends WebApplication
+{    	
+	/**
+	 * @see org.apache.wicket.Application#getHomePage()
+	 */
+	@Override
+	public Class<HomePage> getHomePage()
+	{
+		return HomePage.class;
+	}
+
+	/**
+	 * @see org.apache.wicket.Application#init()
+	 */
+	@Override
+	public void init()
+	{
+		super.init();
+
+		// add your configuration here
+	}
+}
