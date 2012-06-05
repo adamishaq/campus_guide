f666a483ce27b00b973f8dc5ff9f01d2b7528294
Adam Ishaq <adam.ishaq09@imperial.ac.uk>
2012-06-05 23:04:36 +0100

    Major overhaul of Places API

--- src/main/java/uk/ac/ic/doc/campusProject/web/servlet/PlacesApi.java
+++ src/main/java/uk/ac/ic/doc/campusProject/web/servlet/PlacesApi.java
@@ -1,6 +1,7 @@
 package uk.ac.ic.doc.campusProject.web.servlet;
 
 import java.io.IOException;
+import java.security.InvalidParameterException;
 import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -25,18 +26,19 @@ public class PlacesApi extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
 		Connection conn = DatabaseConnectionManager.getConnection("live");
 		PreparedStatement stmt;
-		String verbose = request.getParameter("verbose");
-		String building = request.getParameter("building");
-		String floor = request.getParameter("floor");
-		String room = request.getParameter("room");
-		String left = request.getParameter("left");
-		String right = request.getParameter("right");
-		String top = request.getParameter("top");
-		String bottom = request.getParameter("bottom");
-		
+		String requestUri = request.getRequestURI().replaceFirst("/", "").trim();
+		String[] uri = requestUri.split("/");
 		try {
-			if (verbose.equals("F")) {
-				if (left == null || right == null || top == null || bottom == null) {
+			if (!(uri[0].equals("api-places"))) {
+				log.info("Issue");
+				throw new InvalidParameterException("Bad first parameter passed");
+			}
+			else {
+				String requestType = uri[1];
+				log.info(requestType);
+				if (requestType.equals("pixels")) {
+					String building = uri[2];
+					String floor = uri[3];
 					stmt = conn.prepareStatement("SELECT Number, Xpixel, Ypixel FROM Room LEFT JOIN Floor_Contains ON Number=Room AND Room.Building=Floor_Contains.Building LEFT JOIN Building ON Floor_Contains.Building=Building.Name WHERE Building.ShortCode=? AND Floor=?");
 					stmt.setString(1, building);
 					stmt.setString(2, floor);
@@ -55,7 +57,9 @@ public class PlacesApi extends HttpServlet {
 							jsonArray.append(jsonObject.toString());
 							jsonArray.append(",");
 						}
-						jsonArray = jsonArray.replace(jsonArray.length() - 1, jsonArray.length(), "");
+						if (jsonArray.length() > 1) {
+							jsonArray = jsonArray.replace(jsonArray.length() - 1, jsonArray.length(), "");
+						}
 						jsonArray.append("]");
 						os.write(jsonArray.toString().getBytes());
 						response.setStatus(HttpServletResponse.SC_OK);
@@ -63,71 +67,139 @@ public class PlacesApi extends HttpServlet {
 					}
 					conn.close();
 				}
-				else {
-					
+				else if (requestType.equals("roominfo")) {
+					String building = uri[2];
+					String floor = uri[3];
+					if (uri.length == 4) {
+						stmt = conn.prepareStatement("SELECT Number, Type, Description, Room.Building FROM Room LEFT JOIN Floor_Contains ON Number=Room AND Room.Building=Floor_Contains.Building LEFT JOIN Building ON Floor_Contains.Building=Building.Name WHERE Building.ShortCode=? AND Floor=?");
+						stmt.setString(1, building);
+						stmt.setString(2, floor);
+						if (stmt.execute()) {
+							ResultSet rs = stmt.getResultSet();
+							response.setContentType("application/json");
+							ServletOutputStream os = response.getOutputStream();
+							StringBuilder jsonArray = new StringBuilder();
+							jsonArray.append("[");
+							while(rs.next()) {
+								JSONObject jsonObject = new JSONObject();
+								String number = rs.getString("Number");
+								jsonObject.put("number", number);
+								jsonObject.put("type", rs.getString("Type"));
+								jsonObject.put("description", rs.getString("Description"));
+								jsonObject.put("image", new byte[]{});
+								jsonObject.put("building", rs.getString("Building"));
+								jsonArray.append(jsonObject.toString());
+								jsonArray.append(",");
+
+							}
+							if (jsonArray.length() > 1) {
+								jsonArray = jsonArray.replace(jsonArray.length() - 1, jsonArray.length(), "");
+							}
+							jsonArray.append("]");
+							os.write(jsonArray.toString().getBytes());
+							response.setStatus(HttpServletResponse.SC_OK);
+							os.flush();
+						}
+						conn.close();
+					}
+					else {
+						String room = uri[4];
+						stmt = conn.prepareStatement("SELECT Number, Type, Description, Room.Building FROM Room LEFT JOIN Floor_Contains ON Room.Number=Floor_Contains.Room AND Room.Building=Floor_Contains.Building LEFT JOIN Building ON Floor_Contains.Building=Building.Name WHERE Building.ShortCode=? AND Floor=?" +
+								" AND Room.Number= ?");
+						stmt.setString(1, building);
+						stmt.setString(2, floor);
+						stmt.setString(3, room);
+						if (stmt.execute()) {
+							ResultSet rs = stmt.getResultSet();
+							response.setContentType("application/json");
+							ServletOutputStream os = response.getOutputStream();
+							JSONObject jsonObject = new JSONObject();
+							while(rs.next()) {
+								String number = rs.getString("Number");
+								jsonObject.put("number", number);
+								jsonObject.put("type", rs.getString("Type"));
+								jsonObject.put("description", rs.getString("Description"));
+								jsonObject.put("image", new byte[]{});
+								jsonObject.put("building", rs.getString("Building"));
+
+							}
+							os.write(jsonObject.toString().getBytes());
+							response.setStatus(HttpServletResponse.SC_OK);
+							os.flush();
+						}
+						conn.close();
+					}
 				}
-			}
-			else if (verbose.equals("T")) {
-				if ((left == null || right == null || top == null || bottom == null) && room != null) {
-					stmt = conn.prepareStatement("SELECT Number, Type, Description, Image, Room.Building FROM Room LEFT JOIN Floor_Contains ON Room.Number=Floor_Contains.Room AND Room.Building=Floor_Contains.Building LEFT JOIN Building ON Floor_Contains.Building=Building.Name WHERE Building.ShortCode=? AND Floor=?" +
+				else if (requestType.equals("image")) {
+					String building = uri[2];
+					String floor = uri[3];
+					String room = uri[4];
+					stmt = conn.prepareStatement("SELECT Image FROM Room LEFT JOIN Floor_Contains ON Room.Number=Floor_Contains.Room AND Room.Building=Floor_Contains.Building LEFT JOIN Building ON Floor_Contains.Building=Building.Name WHERE Building.ShortCode=? AND Floor=?" +
 							" AND Room.Number= ?");
 					stmt.setString(1, building);
 					stmt.setString(2, floor);
 					stmt.setString(3, room);
-					log.info(building + "," + floor + "," + room);
 					if (stmt.execute()) {
 						ResultSet rs = stmt.getResultSet();
 						response.setContentType("application/json");
 						ServletOutputStream os = response.getOutputStream();
-						JSONObject jsonObject = new JSONObject();
 						while(rs.next()) {
-							String number = rs.getString("Number");
-							jsonObject.put("number", number);
-							jsonObject.put("type", rs.getString("Type"));
-							jsonObject.put("description", rs.getString("Description"));
 							Blob blob = rs.getBlob("Image");
 							if (blob == null) {
-								jsonObject.put("image", new byte[]{});
+								//UPLOAD DUMMY IMAGE 
+								os.write(new byte[]{});
 							}
 							else {
-								jsonObject.put("image", blob.getBytes(1, (int)blob.length()));
+								os.write(blob.getBytes(1, (int)blob.length()));
 							}
-							jsonObject.put("building", rs.getString("Building"));
-							
 						}
-						os.write(jsonObject.toString().getBytes());
 						response.setStatus(HttpServletResponse.SC_OK);
 						os.flush();
 					}
 					conn.close();
 				}
-				else if (room == null){
-					stmt = conn.prepareStatement("SELECT Number, Type, Description, Image, Room.Building FROM Room LEFT JOIN Floor_Contains ON Number=Room AND Room.Building=Floor_Contains.Building LEFT JOIN Building ON Floor_Contains.Building=Building.Name WHERE Building.ShortCode=? AND Floor=?");
-					stmt.setString(1, building);
-					stmt.setString(2, floor);
+				else if (requestType.equals("buildinglist")) { 
+					stmt = conn.prepareStatement("SELECT Name FROM Building");
 					if (stmt.execute()) {
 						ResultSet rs = stmt.getResultSet();
-						response.setContentType("application/json");
 						ServletOutputStream os = response.getOutputStream();
+						response.setContentType("application/json");
 						StringBuilder jsonArray = new StringBuilder();
 						jsonArray.append("[");
 						while(rs.next()) {
 							JSONObject jsonObject = new JSONObject();
-							String number = rs.getString("Number");
-							jsonObject.put("number", number);
-							jsonObject.put("type", rs.getString("Type"));
-							jsonObject.put("description", rs.getString("Description"));
-							Blob blob = rs.getBlob("Image");
-							if (blob == null) {
-								jsonObject.put("image", new byte[]{});
-							}
-							else {
-								jsonObject.put("image", blob.getBytes(1, (int)blob.length()));
-							}
-							jsonObject.put("building", rs.getString("Building"));
+							String building = rs.getString("Name");
+							jsonObject.put("building", building);
 							jsonArray.append(jsonObject.toString());
 							jsonArray.append(",");
-							
+
+						}
+						if (jsonArray.length() > 1) {
+							jsonArray = jsonArray.replace(jsonArray.length() - 1, jsonArray.length(), "");
+						}
+						jsonArray.append("]");
+						os.write(jsonArray.toString().getBytes());
+						response.setStatus(HttpServletResponse.SC_OK);
+						os.flush();
+					}
+				}
+				else if (requestType.equals("floorlist")) {
+					String building = uri[2];
+					stmt = conn.prepareStatement("SELECT Floor.Floor FROM Floor LEFT JOIN Building ON Building.Name=Floor.Building WHERE Building.ShortCode=?");
+					stmt.setString(1, building);
+					if (stmt.execute()) {
+						ResultSet rs = stmt.getResultSet();
+						ServletOutputStream os = response.getOutputStream();
+						response.setContentType("application/json");
+						StringBuilder jsonArray = new StringBuilder();
+						jsonArray.append("[");
+						while(rs.next()) {
+							JSONObject jsonObject = new JSONObject();
+							String floor = rs.getString("Floor.Floor");
+							jsonObject.put("floor", floor);
+							jsonArray.append(jsonObject.toString());
+							jsonArray.append(",");
+
 						}
 						jsonArray = jsonArray.replace(jsonArray.length() - 1, jsonArray.length(), "");
 						jsonArray.append("]");
@@ -135,17 +207,36 @@ public class PlacesApi extends HttpServlet {
 						response.setStatus(HttpServletResponse.SC_OK);
 						os.flush();
 					}
-					conn.close();
+
 				}
-				
+				else {
+					throw new InvalidParameterException("Bad parameters passed");
+				}
+			}	
+		}
+		catch (InvalidParameterException e) {
+			try {
+				log.error(e);
+				response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			} catch (IOException e1) {
+				e1.printStackTrace();
 			}
-			
-		} catch (SQLException | IOException | JSONException e) {
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			try {
+				log.error(e);
+				response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
 			e.printStackTrace();
 		}
-		
-		
-		
 	}
+
 
 }
